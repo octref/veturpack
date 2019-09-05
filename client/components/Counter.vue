a536a1cd621b3731ae330e8803dcf6b536586de5
Pine Wu <octref@gmail.com>
2019-09-05 11:28:56 -0700

    Repro

--- client/components/Counter.vue
+++ client/components/Counter.vue
@@ -1,38 +1,34 @@
 <template>
   <div class="counter-wrapper">
     <div>{{ msg }}</div>
-    <div>
-      <img src="https://vuejs.org/images/logo.png" width="200" height="200" />
-    </div>
-    <div class="counter">
-      {{ count }}
-    </div>
-    <button @click="$store.commit('INCREMENT')">Increment</button>
-    <button @click="$store.commit('DECREMENT')">Decrement</button>
-    <button @click="$store.dispatch('incrementAsync')">Increment Async</button>
-  </div>
+    <div><img src="https://vuejs.org/images/logo.png" width="200" height="200" /></div>
+    <div class="counter">{{ count }}</div>
+    <button @click="$store.commit('INCREMENT');">Increment</button>
+    <button @click="$store.commit('DECREMENT');">Decrement</button>
+    <button @click="$store.dispatch('incrementAsync');">Increment Async</button>
+      </div>
 </template>
 
 <script>
-import * as _ from 'lodash'
+import * as _ from 'lodash';
 
 export default {
-  data () {
+      data() {
     return {
       msg: 'Vetur means "Winter" in icelandic.'
-    }
+    };
   },
   computed: {
-    count () {
-      return this.$store.state.count
+    count() {
+      return this.$store.state.count;
     }
   },
   methods: {
-    hello () {
-      console.log(this.msg)
+    hello() {
+      console.log(this.msg);
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
