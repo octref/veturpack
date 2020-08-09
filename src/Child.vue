89313b44c885fe1510a8ffbd94a5707b5134b9f3
Pine Wu <octref@gmail.com>
2020-08-09 09:37:22 +0800

    Test

---
+++ src/Child.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  props: {
+    foo: {
+      type: Boolean
+    },
+    bar: {
+      type: String
+    }
+  },
+  setup(props) {
+
+  }
+})
+</script>
--- src/Test.vue
+++ src/Test.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    {{ fo }}
+    <child :baz="" ::bar="1"></child>
   </div>
 </template>
 
 <script>
 import * as _ from 'lodash'
+import { defineComponent } from 'vue'
 
-export default {
+import Child from './Child.vue'
+
+export default defineComponent({
+  components: {
+    Component,
+    Child
+  },
   props: ['foo', 'bar']
-}
+})
 </script>
